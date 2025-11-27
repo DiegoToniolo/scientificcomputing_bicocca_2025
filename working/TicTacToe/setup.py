998092e9475a3b854ff659e8b5414045de6ced39
Diego Toniolo <diegotoniolo13@gmail.com>
2025-11-27 12:14:43 +0100

    Setting up the setup.py file

---
+++ working/TicTacToe/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(name="TicTacToe",
+      description="Tic Tac Toe game for SciComp class",
+      url="https://github.com/DiegoToniolo",
+      author="Diego Toniolo",
+      author_email="d.toniolo4@campus.unimib.it",
+      version="0.0.1",
+      packages=find_packages())
